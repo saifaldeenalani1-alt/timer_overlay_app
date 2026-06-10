75a0f62741e6783b934bc7aea77067096256782c
saifaldeenalani1-alt <saifaldeenalani1@gmail.com>
2026-06-10 07:06:08 +0300

    fix: use bool values instead of int in segment patterns

--- lib/widgets/seven_segment.dart
+++ lib/widgets/seven_segment.dart
@@ -30,16 +30,16 @@ class _SevenSegPainter extends CustomPainter {
   _SevenSegPainter({required this.text, required this.color, required this.fontSize});
 
   static const List<List<bool>> _digitPatterns = [
-    [1,1,1,1,1,1,0], // 0
-    [0,1,1,0,0,0,0], // 1
-    [1,1,0,1,1,0,1], // 2
-    [1,1,1,1,0,0,1], // 3
-    [0,1,1,0,0,1,1], // 4
-    [1,0,1,1,0,1,1], // 5
-    [1,0,1,1,1,1,1], // 6
-    [1,1,1,0,0,0,0], // 7
-    [1,1,1,1,1,1,1], // 8
-    [1,1,1,1,0,1,1], // 9
+    [true,true,true,true,true,true,false], // 0
+    [false,true,true,false,false,false,false], // 1
+    [true,true,false,true,true,false,true], // 2
+    [true,true,true,true,false,false,true], // 3
+    [false,true,true,false,false,true,true], // 4
+    [true,false,true,true,false,true,true], // 5
+    [true,false,true,true,true,true,true], // 6
+    [true,true,true,false,false,false,false], // 7
+    [true,true,true,true,true,true,true], // 8
+    [true,true,true,true,false,true,true], // 9
   ];
 
   @override
